5f70bf4e1f7f2917f097b8a9aa6328819c7bcda6
hertzpaul <paularam55@gmail.com>
2024-11-28 08:35:05 +0100

    added to repo

---
+++ handling_multiple_exceptions/Program.cs
@@ -0,0 +1,29 @@
+﻿
+class Program
+{
+    static void Main(string[] args)
+    {
+        HandleMultipleExceptions("2", 1);
+        HandleMultipleExceptions("abc", 1);
+        HandleMultipleExceptions("2", 5); 
+    }
+
+    static void HandleMultipleExceptions(string inputString, int index)
+    {
+        int[] numbers = { 1, 2, 3 };
+
+        try
+        {
+            int parsedValue = int.Parse(inputString);
+            Console.WriteLine(numbers[index]);
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid format");
+        }
+        catch (IndexOutOfRangeException)
+        {
+            Console.WriteLine("Index out of range");
+        }
+    }
+}
